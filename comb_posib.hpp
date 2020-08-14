57c14e22224b77e62ea0811f633891a0ac79fece
nemui_sen <40149702+NemuiSen@users.noreply.github.com>
2020-08-14 15:57:59 -0500

    2.0
    unos pequeños cambios a la función quitando los if's para usar otra metodologia un poco mas simple, tambien cree un namespace para las versiónes antiguas de la función

--- comb_posib.hpp
+++ comb_posib.hpp
@@ -1,90 +1,146 @@
-#ifndef comb_hpp
-#define comb_hpp
-
-#include <math.h> //pow
+#pragma once
 #include <iostream> //imprimer mensajes
-#include <vector> //es mejor que los punteros
-using namespace std;
+#include <vector>   //es mejor que los punteros
+#include <math.h>   //pow
 
 namespace nemui
-{    
-    template<typename T, size_t n>
-    void comb_posib_nr_s (T (&elementos)[n]) //combinaciones posibles estaticas sin retorno
-    {
-        cout << "total: " << pow(n, 4) << endl; //el total
-        int it = 0; //iteracion
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                for (int k = 0; k < n; k++)
-                {
-                    for (int l = 0; l < n; l++)
-                    {
-                        cout << "out: "<<elementos[i]<<elementos[j]<<elementos[k]<<elementos[l]<<"  #:" << ++it << endl;
-                    }
-                }
-            }
-        }
-    }
+{
+	// cominaciones posibles no estaticas versión 2.0
+	template <typename t>
+	std::vector<std::vector<t>> comb_posib(std::vector<t> ele, uint32_t cant)
+	{
+		std::vector<std::vector<t>> out;
 
-    template <typename t>
-    vector<vector<t>> comb_posib_s (vector<t> ele) //combinaciones posibles estaticas con retorno
-    {
-        int it = 0; //iterador
-        vector<vector<t>> out; //variable de salida
-        out.resize(ele.size()*ele.size()); //establecer el tamaño de la salida
-        for (int i = 0; i < ele.size(); i++) //primer elemeto
-        {
-            for (int j = 0; j < ele.size(); j++) //segundo elemento
-            {
-                for (int k = 0; k < ele.size(); j++) //ultimo elemento en implementarse
-                {
-                    out.at(it).push_back(ele.at(i)); //poner el primer elemento
-                    out.at(it).push_back(ele.at(j)); //poner el segundo elemento
-                    out.at(it).push_back(ele.at(k)); //poner el ultimo elemento
-                    it++; //el iterador aumenta
-                }
-            }
-        }
-        return out; //retorna el resultado final
-    }
+		if (cant <= 1)
+		{
+			out.resize(ele.size());
+			for (int i = 0; i < out.size(); i++)
+				out.at(i).push_back(ele.at(i));
+			return out;
+		}
+		else
+		{
+			//numero el total de elementos
+			//(numero de elementos presentes ^ la cantidad de elementos que queremos)
+			out.resize(pow(ele.size(), cant));
 
-    template<typename t>
-    vector<vector<t>> comb_posib (vector<t> ele, int cant) // cominaciones posibles no estaticas
-    {
-		vector<vector<t>> out; //valor de salida
-		out.resize(pow(ele.size(), cant)); //establecer como tamaño el total de elementos (numero de elementos ^ la cantidad de elementos)
-        
-		if (cant <= 1) //si la cantidad es menor o igual a uno retorna los elementos separados en vectores distintos
-        {
-            for (int i = 0; i < out.size(); i++)
-            {
-                out.at(i).push_back(ele.at(i)); //guarda el elemento
-            }
-        }
-        else //de lo contrario
-        {
+			std::vector<std::vector<t>> last = comb_posib(ele, cant - 1);
+			int it_e = 0;
+			int it_l = 0;
 
-			int it_t = 0, // iteracion del elemento temporal
-                it_e = 0; // iteracion del los elementos dados
-            vector<vector<t>> temp = comb_posib(ele, cant - 1); //elemento temporal
-            for (int i = 0; i < out.size(); i++)
-            {
-                out.at(i).push_back(ele.at(it_e)); //poner el primer elemento
-                for (int j = 0; j < temp.at(it_t).size(); j++)
-                {
-                    out.at(i).push_back(temp.at(it_t).at(j)); //poner el o los siguientes elementos
-                }
-                it_t++; // el iterator del temporal ahumenta
-                if (it_t >= temp.size()) { it_t = 0; it_e++; } //si el iterador temporal es mayor al tamaño del temporal el it = 0 y el it del elemento ahumenta
-                if (it_e >= ele.size())  it_e = 0; // si el it del elemento es mayor a el it = 0
-        		
-            }
-        }
+			for (int i = 0; i < out.size(); i++)
+			{
+				it_e = i % ele.size();
+				it_l = (i/ele.size())%last.size();
 
-		return out; //retorna el valor de salida
-    }
-} // namespace owo
+				out.at(i).insert(out.at(i).end(), last.at(it_l).begin(), last.at(it_l).end());
+				out.at(i).push_back(ele.at(it_e));
+			}
+			return out;
+		}
+	}
+} // namespace nemui
 
-#endif //comb_hpp
+
+
+
+
+
+
+
+
+
+
+
+
+namespace nemui
+{
+	namespace olds
+	{
+		template<typename T, size_t n>
+		void comb_posib_nr_s (T (&elementos)[n]) //combinaciones posibles estaticas sin retorno
+		{
+			std::cout << "total: " << pow(n, 4) << std::endl; //el total
+			int it = 0; //iteracion
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					for (int k = 0; k < n; k++)
+					{
+						for (int l = 0; l < n; l++)
+						{
+							std::cout << "out: "<<elementos[i]<<elementos[j]<<elementos[k]<<elementos[l]<<"  #:" << ++it << std::endl;
+						}
+					}
+				}
+			}
+		}
+
+
+
+
+
+
+		//combinaciones posibles estaticas con retorno
+		template <typename t>
+		std::vector<std::vector<t>> comb_posib_s (std::vector<t> ele)
+		{
+			int it = 0; //iterador
+			std::vector<std::vector<t>> out; //variable de salida
+			out.resize(ele.size()*ele.size()); //establecer el tamaño de la salida
+			for (int i = 0; i < ele.size(); i++) //primer elemeto
+			{
+				for (int j = 0; j < ele.size(); j++) //segundo elemento
+				{
+					for (int k = 0; k < ele.size(); j++) //ultimo elemento en implementarse
+					{
+						out.at(it).push_back(ele.at(i)); //poner el primer elemento
+						out.at(it).push_back(ele.at(j)); //poner el segundo elemento
+						out.at(it).push_back(ele.at(k)); //poner el ultimo elemento
+						it++; //el iterador aumenta
+					}
+				}
+			}
+			return out; //retorna el resultado final
+		}
+
+
+
+
+		// cominaciones posibles no estaticas versión 1.0
+		template <typename t>
+		std::vector<std::vector<t>> comb_posib_1 (std::vector<t> ele, int cant)
+		{
+			std::vector<std::vector<t>> out(pow(ele.size(), cant));
+			//numero el total de elementos (numero de elementos ^ la cantidad de elementos)
+
+			if (cant > 1)
+			{
+				int it_t = 0; // iteracion del elemento temporal
+				int	it_e = 0; // iteracion del los elementos dados
+				std::vector<std::vector<t>> temp = comb_posib_1(ele, cant - 1); //elemento temporal
+				for (int i = 0; i < out.size(); i++)
+				{
+					out.at(i).push_back(ele.at(it_e)); //poner el primer elemento
+					for (int j = 0; j < temp.at(it_t).size(); j++)
+					{
+						out.at(i).push_back(temp.at(it_t).at(j)); //poner el o los siguientes elementos
+					}
+
+					it_t++; // el iterator del temporal ahumenta
+					if (it_t >= temp.size()) { it_t = 0; it_e++; } //si el iterador temporal es mayor al tamaño del temporal el it = 0 y el it del elemento ahumenta
+					if (it_e >= ele.size())  it_e = 0; // si el it del elemento es mayor a el it = 0
+				}
+				return out;
+			}
+
+			for (int i = 0; i < out.size(); i++)
+			{
+				out.at(i).push_back(ele.at(i)); //guarda el elemento
+			}
+
+			return out; //retorna el valor de salida
+		}
+	} // namespace olds
+} // namespace nemui
--- main.cpp
+++ main.cpp
@@ -1,24 +1,28 @@
-#include <conio.h>
 #include <iostream>
+#include <string>
 #include "comb_posib.hpp"
-
-using namespace std;
 
 int main()
 {
-    vector<char> uwu = {'0', '1'}; //los elementos
-    vector<vector<char>> owo = nemui::comb_posib(uwu, 2); //la funcion para obtener las combinaciones posibles
-    cout << "combinaciones posibles de los elementos: 0, 1" << endl;
-    cout << "con un tamaño de 3 elementos" << endl;
-    for (auto it_x: owo) //obtenemos los elementos de las filas de la matriz
-    {
-        for (auto it_y: it_x) //obtenemos el elemento de la fila
-        {
-            cout << it_y; //ahora si imprimimos el elemento que en este caso el un char
-        }
-        cout << endl; //un salto de linea para que no se vea todo apilado
-    }
+	std::cout << "Ponga los elementos que quiere prosesar: ";
+	std::string input;
+	std::getline(std::cin, input);
+	std::cout << "Ponga el numero de elemento por cada fila que quiere: ";
+	int n;
+	std::cin >> n;
 
-    getch(); //para que no se cierre la consola al instante
-    return 0;
+	std::vector<char> v(input.begin(), input.end());
+	std::vector<std::vector<char>> result = nemui::comb_posib(v, n);
+	std::cout << "resultado" << std::endl;
+	for (auto it_x: result) //obtenemos los elementos de las filas de la matriz
+	{
+		for (auto it_y: it_x) //obtenemos el elemento de la fila
+		{
+			std::cout << it_y; //ahora si imprimimos el elemento que en este caso el un char
+		}
+		std::cout << std::endl; //un salto de linea para que no se vea todo apilado
+	}
+
+	std::cin.get(); //para que no se cierre la consola al instante
+	return 0;
 }
